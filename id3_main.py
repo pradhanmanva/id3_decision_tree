d5ef8b4e894426f18903960cd8d94420693b5552
Manva Pradhan <pradhanmanva@gmail.com>
2017-09-21 16:32:57 -0500

    adding read_func()

--- id3_main.py
+++ id3_main.py
@@ -1,3 +1,10 @@
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
+import sys
 
+if __name__== "__main__":
+    cmdLine = sys.argv
+    print(len(sys.argv))
+
+    trainData = read_file(cmdLine[0])
+    testData = read_file(cmdLine[1])
